fdc142daa7baceb6717e36996bd73d061ed99b86
Robert Olson <robert@highest.technology>
2017-09-16 13:37:28 -0700

    added basic proof of concept projectile shooter

---
+++ Assets/_core/Scripts/fish_shooter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fist_shooter : MonoBehaviour {
+
+	public GameObject prefab;
+	// Use this for initialization
+	void Start () {
+		prefab = Resources.Load ("fish_treat") as GameObject;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			GameObject projectile = Instantiate (prefab) as GameObject;
+			projectile.transform.position = transform.position + Camera.main.transform.forward * 2;
+			Rigidbody rb = projectile.GetComponent<Rigidbody> ();
+			rb.velocity = Camera.main.transform.forward * 30;
+		}
+	}
+}
